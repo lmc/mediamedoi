72eba2706cd8b2d763f63c124e799d1edf4f7db0
Luke Mcildoon <luke@twofiftyfive.net>
2011-03-02 19:53:47 +1100

    working linereader for handbrake stdout output

--- app/models/converter.rb
+++ app/models/converter.rb
@@ -3,7 +3,7 @@ class Converter
   BIN_PATH = '/Applications/HandBrakeCLI'
   OUTPUT_PATH = '/Users/luke/Documents/Converted Videos/'
   DEFAULT_OPTIONS = {
-    
+    :preset => "\"iPhone & iPod Touch\""
   }
   
   def self.convert(media_library_file)
@@ -11,8 +11,22 @@ class Converter
     output = File.join(OUTPUT_PATH,media_library_file.name)
     options = make_options({:input => esc(input),:output => esc(output)}.merge(DEFAULT_OPTIONS))
     cmd = "#{BIN_PATH} #{options}"
-    #stdin, stdout, stderr = Open3.popen3(cmd)
-    #[stdin,stdout,stderr]
+    puts cmd
+    puts "---"
+    puts
+    
+    progress_line = ""
+    buffer = ""
+    Open3.popen3(cmd) do |stdin,stdout,stderr|
+      while chr = stdout.readchar
+        unless chr == "\r"
+          buffer << chr
+        else
+          progress_line, buffer = buffer, ""
+          puts progress_line
+        end
+      end
+    end
   end
   
   def self.make_options(options_hash)
